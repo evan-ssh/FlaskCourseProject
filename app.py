268ad2a040e33aa195100070c6fd0d532042ca24
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-04-16 18:48:55 -0230

    Construct Homepage and Get Collections Total Price

--- app.py
+++ app.py
@@ -4,3 +4,30 @@ import os
 import uuid
 
 app = Flask(__name__)
+
+#Session Settings
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+file_save_location = "static/images"
+allowed_types = [".png", ".jpg"]
+
+#Homepage
+@app.route("/", methods=["GET"])
+def index():
+    if "videoGames" not in session:
+        print("clearing games")
+        session["videoGames"] = []
+    print(session.get("videoGames"))
+    total_value = 0.0
+    for game in session.get("videoGames"):
+        try:
+            total_value += float(game.get("price", 0))
+        except (ValueError, TypeError):
+            print(f"Price entered was invalid for {game}")
+    return render_template("index.html", games=session.get("videoGames"), file_location=file_save_location,total_value=total_value)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
