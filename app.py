25f3a3537e8e9b133e386fda299d36221eaecafb
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-04-16 18:57:24 -0230

    Construct page to add a game to the collection

--- app.py
+++ app.py
@@ -28,6 +28,34 @@ def index():
             print(f"Price entered was invalid for {game}")
     return render_template("index.html", games=session.get("videoGames"), file_location=file_save_location,total_value=total_value)
 
+#Add a game
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "GET":
+        return render_template("add.html")
+    elif request.method == "POST":
+        if "videoGames" not in session:
+            print("Clearing session data")
+            session["videoGames"] = []
+        title = request.form.get("title", "invalid")
+        plat = request.form.get("plat", "invalid")
+        year = request.form.get("year", "invalid")
+        price = request.form.get("price", "0.00")
 
+        uploadedFile = request.files['file']
+        if uploadedFile.filename != '':
+            extension = os.path.splitext(uploadedFile.filename)[1]
+            if extension in allowed_types:
+                uniqueName = f"{uuid.uuid4().hex}{extension}"
+                filename = os.path.join(file_save_location, uniqueName)
+                uploadedFile.save(filename)
+                session["videoGames"].append({"title": title, "plat": plat, "year": year, "price": price, "image": uniqueName})
+            else:
+                flash("The file is of the wrong type", "error")
+                return redirect("./add")
+        print(session.get("videoGames"))
+        session.modified = True
+        flash("Good job! You have added a new game to your collection", "message")
+        return redirect("/")
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
